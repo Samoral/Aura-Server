65505ca6819de541608d90e228fd1d78565d188e
samztechng <90157690+Samoral@users.noreply.github.com>
2026-03-19 19:57:38 +0100

    Use environment variable for server port

--- index.js
+++ index.js
@@ -352,7 +352,7 @@ FINAL OUTPUT RULE
 });
 
 
-const PORT = 10000;
+const PORT = process.env.PORT||10000;
 app.listen(PORT,"0.0.0.0", () => {
     console.log(`✅ Server running on http://localhost:${PORT}`);
 });
